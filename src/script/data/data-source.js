ce476a358fca5cd6d4b62a5d53af9c597ec13157
gentamahardika <69895649+gentamahardika@users.noreply.github.com>
2020-09-14 19:56:04 +0800

    Update data-source.js

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,20 +1,21 @@
-import series from './series.js';
 class DataSource {
 
     static searchSeries(query) {
-        return fetch(`https://api.themoviedb.org/3/search/tv?api_key=450b5607a65f55b1ed5f82b4a0187749&language=en-US&page=1&query=Riverdale&include_adult=false}`)
+        return fetch(`https://api.themoviedb.org/3/search/tv?api_key=450b5607a65f55b1ed5f82b4a0187749&query=${query}&include_adult=false`)
             .then(response => {
                 return response.json();
             })
 
             .then(responseJson => {
-                if (responseJson.series) {
-                    return Promise.resolve(responseJson.series);
+                if (responseJson.results) {
+                    return Promise.resolve(responseJson.results);
                 } else {
                     return Promise.reject(`${query} is not found`);
                 }
             })
     }
+
 }
+
 
 export default DataSource;
